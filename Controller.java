121263a17dca9c0a3c1406897d9b2b14dcf18382
Zhen Chen <me@zhen-chen.com>
2016-06-08 02:25:26 +1200

    Add menu

--- Controller.java
+++ Controller.java
@@ -1,7 +1,6 @@
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
@@ -18,16 +17,20 @@ import java.io.IOException;
  */
 
 public class Controller {
+	// at least 1 thread
 	private static final int NUMBER_OF_THREADS = 1;
+	// location of the downloading engine
 	private static final String LOCATION = "E:/软件/You-Get/";
-	// Windows platform uses GBK in Chinese version
+	// Windows platform uses GBK as charset in Chinese version
 	private static final String CHARSET = "GBK";
 	private static final BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-	private static Map<Thread, YouGet> threadPool = new HashMap<Thread, YouGet>(NUMBER_OF_THREADS);
-	private static List<String> targetList = new ArrayList<String>();
-	private static Map<String, String> targetMap = new HashMap<String, String>();
-	private static int targetsNumber = 0;
-	private static int activeThreadsNumber = 0;
+	private static Set<Thread> threadPool = new HashSet<Thread>();
+	private static Set<YouGet> processSet = new LinkedHashSet<YouGet>();
+	private static Set<YouGet> failedProcessSet = new HashSet<YouGet>();
+
+	private static enum Choice {
+		INPUT, DOWNLOAD, TITLE, LOAD, SAVE, EXIT;
+	}
 
 	/**
 	 * Get all target URLs from user.
@@ -40,68 +43,120 @@ public class Controller {
 		String line;
 		System.out.println("Please enter all target URLs, one line for each:");
 		while (!(line = input.readLine()).equals("")) {
-			targetList.add(line);
+			YouGet yg = new YouGet(line);
+			processSet.add(yg);
 		}
-		targetsNumber = targetList.size();
-		System.out.format("%d targets added.%n", targetsNumber);
+		System.out.printf("%d URLs in total.%n%n", processSet.size());
 	}
 
-	private static List<YouGet> clearThreadPool() throws InterruptedException {
-		List<YouGet> ProcessList = new ArrayList<YouGet>();
-		for (Thread t : threadPool.keySet()) {
-			t.join();
-			YouGet yg = threadPool.get(t);
+	private static void threadDistribute(YouGet.Task t) {
+		for (YouGet yg : processSet) {
+			if (threadPool.size() == NUMBER_OF_THREADS) {
+				clearThreadPool();
+			}
+			yg.setTask(t);
+			threadPool.add(yg);
+			yg.start();
+		}
+		clearThreadPool();
+		reportFailure();
+	}
+
+	private static void clearThreadPool() {
+		for (Thread t : threadPool) {
+			try {
+				t.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 			threadPool.remove(t);
-			activeThreadsNumber--;
-			if (yg.getState()) {
-				ProcessList.add(yg);
+			YouGet yg = (YouGet) t;
+			if (!yg.isSuccess()) {
+				failedProcessSet.add(yg);
 			}
-		}
-		return ProcessList;
-	}
-
-	private static void addTitleFromThreadPool(List<YouGet> ProcessList) {
-		for (YouGet yg : ProcessList) {
-			targetMap.put(yg.getTarget().toString(), yg.getFilename());
 		}
 	}
 
-	private static void getAllTitles()
-			throws InterruptedException, NoExecutableSetException, IOException, ProcessErrorException {
-		while (!targetList.isEmpty()) {
-			if (activeThreadsNumber < NUMBER_OF_THREADS) {
-				YouGet yg = new YouGet(targetList.get(0));
-				targetList.remove(0);
-				yg.setTask(YouGet.Task.INFO);
-				Thread t = new Thread(yg);
-				threadPool.put(t, yg);
-				t.start();
-				activeThreadsNumber++;
-			} else {
-				addTitleFromThreadPool(clearThreadPool());
+	private static void reportFailure() {
+		for (YouGet yg : failedProcessSet) {
+			System.out.printf("%s failed in task %s.%n", yg.getTarget().toString(), yg.getTask().toString());
+		}
+		if (failedProcessSet.size() > 0) {
+			System.out.println();
+		}
+	}
+
+	private static Choice displayMenu() throws IOException {
+		System.out.println("Menu:");
+		System.out.println("1. Input target URLs");
+		System.out.println("3. Show all titles");
+		System.out.println("6. Exit");
+
+		Choice choice = null;
+		boolean again = true;
+		while (again) {
+			switch (input.readLine()) {
+			case "1":
+				choice = Choice.INPUT;
+				again = false;
+				break;
+			case "3":
+				choice = Choice.TITLE;
+				again = false;
+				break;
+			case "6":
+				choice = Choice.EXIT;
+				again = false;
+				break;
 			}
 		}
-		addTitleFromThreadPool(clearThreadPool());
+
+		return choice;
+	}
+
+	private static void displayTitle() {
+		threadDistribute(YouGet.Task.INFO);
+		for (YouGet yg : processSet) {
+			System.out.printf("%s  %s%n", yg.getTitle(), yg.getTarget().toString());
+		}
+		if (processSet.size() > 0) {
+			System.out.println();
+		}
+	}
+
+	private static void displayExit() {
+		System.out.println("Exit. Thank you!");
 	}
 
 	public static void main(String[] args) {
 		try {
 			YouGet.setExecutable(LOCATION);
 			YouGet.setCharset(CHARSET);
-			getInput();
-			getAllTitles();
-			for (String target : targetMap.keySet()) {
-				System.out.println(target + ": " + targetMap.get(target));
-			}
+
+			boolean again = true;
+			do {
+				switch (displayMenu()) {
+				case INPUT:
+					getInput();
+					break;
+				case DOWNLOAD:
+					break;
+				case TITLE:
+					displayTitle();
+					break;
+				case SAVE:
+					break;
+				case LOAD:
+					break;
+				case EXIT:
+					again = false;
+					displayExit();
+					break;
+				}
+			} while (again);
 		} catch (NoExecutableFileFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		} catch (NoExecutableSetException e) {
-			e.printStackTrace();
-		} catch (ProcessErrorException e) {
 			e.printStackTrace();
 		}
 	}
